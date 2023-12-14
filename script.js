2f4294d8717b9c402197d501f5f9a0f0ffa6b9ff
Vinh <tranthanhvinhlxag1995@gmail.com>
2023-12-15 00:08:49 +0700

    update Short-circuiting

--- script.js
+++ script.js
@@ -31,8 +31,11 @@ const restaurant = {
   },
   orderPasta: function(ing1, ing2, ing3){
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+  orderPizza: function(mainIngredient, ...orther){
+      console.log(mainIngredient);
+      console.log(orther);
   }
-  
 };
 
 /*---Destructuring Object--*/
@@ -86,9 +89,71 @@ const [i, , [j, k]] = nested;
 console.log(i, j, k);
 */
 /*--- SPREAD OPTERATOR ---*/
-const ingredients = [
-  prompt(`Let\'s make pasta with ingredient 1`),
-  prompt(`ingredient 2`),
-  prompt(`ingredient 3`)
-];
-restaurant.orderPasta(...ingredients);
+// const ingredients = [
+//   prompt(`Let\'s make pasta with ingredient 1`),
+//   prompt(`ingredient 2`),
+//   prompt(`ingredient 3`)
+// ];
+// restaurant.orderPasta(...ingredients);
+
+/*----REST PATTERN & PARAMETER----*/
+
+/*-Array-*/
+// const [pizza, , risotto, ...otherMenus] = [...restaurant.starterMenu, restaurant.mainMenu];
+// console.log(pizza, risotto, otherMenus);
+
+/*object*/
+
+// const {thu, ...weekDays} = restaurant.openingHours;
+// console.log(weekDays);
+
+/*function*/
+
+// const add = function(...num){
+//   let sum = 0;
+//   for(let i = 0; i < num.length; i++){
+//     sum += num[i];
+//   }
+//   console.log(sum);
+// };
+// add(1,3,7,9);
+
+// restaurant.orderPizza(`orange`, `butter`, `sperm`, `mushroom`, `peanut`);
+
+/*---SHORT CIRCUITING (&& AND ||)---*/
+
+/*-|| operator (OR)-*/
+
+//chcking if a property exists
+
+//const guest = restaurant.numGuest ? console.log(restaurant.Guest) : console.log(1); // 1
+//explain: if there is a property called numGuest on restaurant object (that mean restaurant.numGuest has truethy value), display that value on the console,. If not, display 1 which is default value we set.
+
+//Use short-circuiting:
+
+// const guest = restaurant.numGuest || 1;
+// console.log(guest); // 1
+
+/*Exp: say we are getting some data from a remote services/database about our online store customers.
+Some of them are VIPs and some are not. Those are not VIPs do not have a vip property on the object.*/
+
+// const customer1 = {
+//   name: `Vinh`,
+//   age: 29,
+//   vip: true,
+// };
+// const customer2 = {
+//   name: `Phuong`,
+//   age: 27,
+// };
+// const isVip = customer => {
+//   const vip = customer.vip || false;
+//   return `${customer.name} is ${vip ? `a VIP` : `not a VIP`}`;
+// };
+// console.log(isVip(customer1)); //Vinh is a VIP
+// console.log(isVip(customer2));//Phuong is not a VIP
+
+/*-Operator && (AND)-*/
+
+console.log(3 && 1 && 2 && `Vinh`); // Vinh
+console.log(3 && 1 && null && `Vinh`);// null

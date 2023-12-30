0b06e08221b7d48a3ad72af533aa72f9f8f93271
Vinh <tranthanhvinhlxag1995@gmail.com>
2023-12-30 23:53:45 +0700

    update code challenge #2

--- script.js
+++ script.js
@@ -392,16 +392,25 @@ GOOD LUCK 😀
       Odd of victory Borrussia Dortmund: 6.5
 Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
 */
-for(const [team, value] of Object.entries(game.odds)){
-  console.log(`Odd of ${team === `x` ? `draw: ${value}` : `victory ${game[team]}: ${value}` }`);
-}
+// for(const [team, value] of Object.entries(game.odds)){
+//   console.log(`Odd of ${team === `x` ? `draw: ${value}` : `victory ${game[team]}: ${value}` }`);
+// }
 /*
-BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+BONUS: Create an object called 'scorers' which contains the names of the players 
+who scored as properties, and the number of goals as the value. 
+In this game, it will look like this:
       {
         Gnarby: 1,
         Hummels: 1,
         Lewandowski: 2
       }
 */
+// let scorer = {};
+// for(const player of game.scored){
+//  scorer[player] ? scorer[player]++ : (scorer[player] = 1);
+// }
+// console.log(scorer);
+
+
 
 
